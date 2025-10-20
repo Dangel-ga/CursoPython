ab55fca95085330ef1f7bf5dac4fb70d195b9387
unknown <dangel-ga@hotmail.com>
2025-10-19 22:33:02 -0600

    archivo inicial

--- mundo.py
+++ mundo.py
@@ -1,11 +1,3 @@
-m="suma de dos números"
+a="mundo"
 
-print(m)
-
-a=int(input("Ingrese un valor: "))
-
-b=int(input("Ingrese un segundo valor: "))
-
-suma=a+b
-
-print("La suma de los dos valores es :", suma)
+print(a)
